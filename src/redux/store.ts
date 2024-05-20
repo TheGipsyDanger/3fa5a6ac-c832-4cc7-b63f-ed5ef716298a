5339a4e9b10fe20ceff39e980fd6e3a8ba8252e7
Renan O Henrique <henriquebasshvf@gmail.com>
2024-05-20 15:50:18 -0300

    feat: create initial struct to use redux and saga on project

---
+++ src/redux/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { reducers } from "./reducers";
+import rootSaga from "./rootSaga";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).prepend(sagaMiddleware),
+});
+
+sagaMiddleware.run(rootSaga);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
